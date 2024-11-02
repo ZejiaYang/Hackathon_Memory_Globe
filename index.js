5f5edf9b7ca004252681401426dfac00e4980f1b
Robert Dancer <robertdancer@ymail.com>
2024-11-02 13:16:03 +0000

    static file serving

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-});
+});*/
+
+app.use(express.static('public/'));
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
