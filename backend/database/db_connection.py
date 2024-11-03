af8f2526af2c97815e4d56f0b6a690169a42d282
Zejia_Yang <zejiayang2003@gmail.com>
2024-11-03 10:41:51 +0000

    zejia:message passing

--- backend/database/db_connection.py
+++ backend/database/db_connection.py
@@ -1,7 +1,5 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-import numpy as np
-from datetime import datetime
 
 TEST = 1
 
@@ -26,33 +24,6 @@ class MongoDBConnection:
     def retrieve_memory(self, query):
         return self.db[self.collection].find(query)
 
-    def compute_connections(self, memory, top_k=5, freshness_weight=0.5, similarity_threshold=0.001):
-        # Fetch all memory nodes from the MongoDB collection
-        all_memory_nodes = list(self.collection.find({}))
-        now = datetime.fromisoformat(memory.timestamp.replace("Z", "+00:00"))
-        node_embedding = np.array(memory.embedding) 
-        similarity_scores = []
-        for node in all_memory_nodes:
-            # Calculate cosine similarity
-            other_embedding = np.array(node['embedding'])
-            similarity = self.cosine_similarity(node_embedding, other_embedding)
-            other = datetime.fromisoformat(node['timestamp'].replace("Z", "+00:00")) 
-            time_difference = (now - other).total_seconds() / (60 * 60 * 24)  # in days
-            freshness_score = max(0, 1 - time_difference / 30) 
-            # Combine similarity and freshness
-            combined_score = (similarity * (1 - freshness_weight)) + (freshness_score * freshness_weight)
-            if combined_score >= similarity_threshold:
-                similarity_scores.append((node['_id'],  node['emotions'], combined_score,))
-        top_similar_nodes = sorted(similarity_scores, key=lambda x: x[2], reverse=True)[:top_k]
-        combined_sum = sum([x[3] for x in top_similar_nodes])
-        top_similar_nodes = {(id, emotion, score/combined_sum) for id, emotion, score in top_similar_nodes}
-        return top_similar_nodes
+    def retrieve_all(self):
+        return list(self.collection.find({}))
 
-    @staticmethod
-    def cosine_similarity(vec_a, vec_b):
-        dot_product = np.dot(vec_a, vec_b)
-        norm_a = np.linalg.norm(vec_a)
-        norm_b = np.linalg.norm(vec_b)
-        if norm_a == 0 or norm_b == 0:
-            return 0.0  # Avoid division by zero
-        return dot_product / (norm_a * norm_b)
--- backend/services/memory_service.py
+++ backend/services/memory_service.py
@@ -1,6 +1,7 @@
 # services/memory_service.py
 from ..database.db_connection import MongoDBConnection
 from message_service import Message
+import numpy as np
 
 class MemoryService:
     def __init__(self, mongo_conn:MongoDBConnection, weight=0.5):
@@ -11,21 +12,39 @@ class MemoryService:
         message = Message(message=memory, timestamp=timestamp)
         message.compute_emotions()
         message.compute_embedding()
-        # connections return a dict {id:[weight, emotios]}
         connections = self.compute_connections(message)
-        # update memory
         neighbours = message.update_memory(connections, self.weight)
-        # return previous relevant history and emotion scores (aggrgate)
         self.add_memory(message)
-        # for the fronEnd - emotions and edge weights - graph visualizatio
         return message.emotions, neighbours
         
     def add_memory(self, memory):
-        # add the meory 
         return self.mongo_conn.insert_memory(memory.get_memory_data())
 
-    def compute_connections(self, memory):
-        return self.mongo_conn.compute_connections(memory)
+    def compute_connections(self, memory, top_k=5, freshness_weight=0.3, similarity_threshold=0.001):
+        all_memory_nodes = self.mongo_conn.retrieve_all()
+        node_embedding = np.array(memory.embedding) 
+        similarity_scores = []
+        for node in all_memory_nodes:
+            other_embedding = np.array(node['embedding'])
+            similarity = self.cosine_similarity(node_embedding, other_embedding)
+            time_difference = (memory.timestamp- node['timestamp']) / (60 * 60 * 24)  # in days
+            freshness_score = max(0, 1 - time_difference / 30) 
+            combined_score = (similarity * (1 - freshness_weight)) + (freshness_score * freshness_weight)
+            if combined_score >= similarity_threshold:
+                similarity_scores.append((node['_id'],  node['emotions'], combined_score,))
+        top_similar_nodes = sorted(similarity_scores, key=lambda x: x[2], reverse=True)[:top_k]
+        combined_sum = sum([x[3] for x in top_similar_nodes])
+        top_similar_nodes = {(id, emotion, score/combined_sum) for id, emotion, score in top_similar_nodes}
+        return top_similar_nodes
+
+    @staticmethod
+    def cosine_similarity(vec_a, vec_b):
+        dot_product = np.dot(vec_a, vec_b)
+        norm_a = np.linalg.norm(vec_a)
+        norm_b = np.linalg.norm(vec_b)
+        if norm_a == 0 or norm_b == 0:
+            return 0.0  #
+        return dot_product / (norm_a * norm_b)
     
     def delete_memory(self, memory):
         pass
